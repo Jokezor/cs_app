0ac9e59bd4c4d33e83d541aa4e88bb85795b7b5f
Jokezor <testifyingfor@gmail.com>
2025-08-28 20:03:14 +0200

    another

--- 2_60.c
+++ 2_60.c
@@ -16,15 +16,6 @@ replace_byte(0x12345678, 0, 0xAB) --> 0x123456AB
 typedef unsigned char *byte_pointer;
 
 unsigned replace_byte_simple(unsigned x, int i, unsigned char b) {
-    /*
-    replace_byte(0x12345678, 2, 0xAB) --> 0x12AB5678
-    replace_byte(0x12345678, 0, 0xAB) --> 0x123456AB
-    */
-    // Hmm, to do this, we could simply convert the unsigned x into a unsigned char pointer.
-    // Then we replace AB?
-    // Lets think of other ways.
-    // We could maybe do a union on these bits? But nope, first the bits for that byte position need to be cleared.
-    // Easiest is to convert.
     assert(i >= 0);
     assert(i <= sizeof(unsigned)-1);
     byte_pointer bytes = (byte_pointer) &x;
@@ -36,11 +27,6 @@ unsigned replace_byte_simple(unsigned x, int i, unsigned char b) {
 unsigned replace_byte(unsigned x, int i, unsigned char b) {
     assert(i >= 0);
     assert(i <= sizeof(unsigned)-1);
-    // We could now clear the bits at index i.
-    // If we xor, we would clear the common bits.
-    // 1 byte has 8 bits. 
-    // Basically, a byte with all 1s, bit shifted i bits.
-    // (i << 3) == i*8;
     unsigned mask = unsigned((0xFF) << (i << 3));
     unsigned pos_byte = (b) << (i << 3);
     return (x & ~mask) | pos_byte;
